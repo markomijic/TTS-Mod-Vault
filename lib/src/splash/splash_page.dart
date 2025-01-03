d9948b670ff23d8db6b1cc33b22da7afbcf76884
Marko Mijić <markomijic4@gmail.com>
2025-01-03 17:02:30 +0100

    Added support for custom tts directory selection at startup in case of failure to find it automatically

--- lib/src/splash/splash_page.dart
+++ lib/src/splash/splash_page.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tts_mod_vault/src/state/provider.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:tts_mod_vault/src/utils.dart';
+import 'package:path/path.dart' as path;
 
 class SplashPage extends HookConsumerWidget {
   const SplashPage({super.key});
@@ -13,14 +18,19 @@ class SplashPage extends HookConsumerWidget {
 
     final ttsDirNotFound = useState(false);
 
+    Future<void> load() async {
+      ttsDirNotFound.value = false;
+      await modsNotifier.loadModsData().then(
+            (value) => context.mounted
+                ? Navigator.of(context).pushReplacementNamed('/mods')
+                : null,
+          );
+    }
+
     useEffect(() {
       WidgetsBinding.instance.addPostFrameCallback((_) async {
         if (await directoriesNotifier.checkIfTtsDirectoryExists()) {
-          modsNotifier.loadModsData().then(
-                (value) => context.mounted
-                    ? Navigator.of(context).pushReplacementNamed('/mods')
-                    : null,
-              );
+          await load();
         } else {
           ttsDirNotFound.value = true;
         }
@@ -33,7 +43,51 @@ class SplashPage extends HookConsumerWidget {
       child: Scaffold(
         body: Center(
           child: ttsDirNotFound.value
-              ? Text('TTS directory not found!')
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    Text(
+                      'Tabletop Simulator directory has not been found, please locate it manually.\nIt should contain folders: DLC, Mods, Saves, Screenshots.',
+                      textAlign: TextAlign.center,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 10,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            String? ttsDir =
+                                await FilePicker.platform.getDirectoryPath();
+
+                            if (ttsDir != null) {
+                              if (await Directory(path.join(ttsDir, 'Mods'))
+                                  .exists()) {
+                                directoriesNotifier.updateTtsDirectory(ttsDir);
+                                await load();
+                              } else {
+                                if (!context.mounted) {
+                                  return;
+                                }
+                              }
+                            } else {
+                              if (!context.mounted) {
+                                return;
+                              }
+
+                              showSnackBar(context, 'Invalid directory');
+                            }
+                          },
+                          child: Text('Select TTS directory'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => exit(0),
+                          child: Text('Exit'),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
               : CircularProgressIndicator(
                   color: Colors.white,
                 ),

d5e9a15888cd64dbc1f4a4c073452d615d2e59f3
Marko Mijić <markomijic4@gmail.com>
2025-05-27 18:00:39 +0200

    Updated version to 0.9.1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   WindowOptions windowOptions = const WindowOptions(
     size: Size(double.infinity, double.infinity),
     minimumSize: Size(1280, 720),
-    title: 'TTS Mod Vault 0.9.1-dev',
+    title: 'TTS Mod Vault 0.9.1',
     center: true,
   );
 

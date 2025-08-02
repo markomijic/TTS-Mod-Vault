bbae00f431d59437fb1facd2cacf32dcc685b814
Marko Mijić <markomijic4@gmail.com>
2025-08-02 09:07:48 +0200

    Updated download mod by id dialog with folder selection

--- lib/src/mods/components/download_mod_by_id_dialog.dart
+++ lib/src/mods/components/download_mod_by_id_dialog.dart
@@ -1,7 +1,9 @@
 import 'dart:convert' show JsonEncoder, json;
 import 'dart:io' show File;
+import 'dart:ui' show ImageFilter;
 
 import 'package:bson/bson.dart' show BsonBinary, BsonCodec;
+import 'package:file_picker/file_picker.dart' show FilePicker;
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart'
     show useTextEditingController, useState;
@@ -9,14 +11,19 @@ import 'package:hooks_riverpod/hooks_riverpod.dart'
     show HookConsumerWidget, WidgetRef;
 import 'package:http/http.dart' as http;
 import 'package:image/image.dart' as img;
+import 'package:path/path.dart' as p;
 import 'package:tts_mod_vault/src/state/provider.dart' show directoriesProvider;
+import 'package:tts_mod_vault/src/utils.dart' show showSnackBar;
 
 class DownloadModByIdDialog extends HookConsumerWidget {
   const DownloadModByIdDialog({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final workshopDir = ref.watch(directoriesProvider).workshopDir;
+    final targetDirectory =
+        useState(p.normalize(ref.read(directoriesProvider).workshopDir));
+    final downloading = useState(false);
+    final textController = useTextEditingController();
 
     Future<void> downloadAndResizeImage(String imageUrl, String modId) async {
       try {
@@ -34,15 +41,12 @@ class DownloadModByIdDialog extends HookConsumerWidget {
           return;
         }
 
-        // First save the original at maximum quality with NO compression
-        final tempPath = '$workshopDir/test/${modId}_temp.png';
+        // First save the original at maximum quality
+        final tempPath = '${targetDirectory.value}/${modId}_temp.png';
         final tempFile = File(tempPath);
-        await tempFile.parent.create(recursive: true);
-
-        // Save with level 0 (no compression) to preserve maximum quality
         await tempFile.writeAsBytes(img.encodePng(
           originalImage,
-          level: 0, // No compression
+          level: 0,
         ));
 
         // Read back the uncompressed image
@@ -61,7 +65,7 @@ class DownloadModByIdDialog extends HookConsumerWidget {
         );
 
         // Save the final resized image with minimal compression
-        final finalPath = '$workshopDir/test/$modId.png';
+        final finalPath = '${targetDirectory.value}/$modId.png';
         final finalFile = File(finalPath);
 
         await finalFile.writeAsBytes(img.encodePng(
@@ -86,121 +90,150 @@ class DownloadModByIdDialog extends HookConsumerWidget {
           return;
         }
 
-        // Create BsonBinary from the response bytes
         final bsonBinary = BsonBinary.from(response.bodyBytes);
-
-        // Deserialize the BSON data
         final decodedData = BsonCodec.deserialize(bsonBinary);
 
         // Convert to pretty-printed JSON
         final jsonString =
             const JsonEncoder.withIndent('  ').convert(decodedData);
 
-        // Save JSON to disk
-        final filePath = '$workshopDir/test/$modId.json'; // TODO remove /test/
+        final filePath = '${targetDirectory.value}/$modId.json';
         final file = File(filePath);
 
-        // Create directory if it doesn't exist
-        await file.parent.create(recursive: true); // TODO remove
-
         await file.writeAsString(jsonString);
-
         debugPrint('JSON saved to: $filePath');
       } catch (e) {
         debugPrint('Error processing BSON file: $e');
       }
     }
 
-    final textController = useTextEditingController();
-    final downloading = useState(false);
-
-    // TODO add blur
-    return AlertDialog(
-      content: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        spacing: 16,
-        children: [
-          Text(
-            'Download Workshop Mod by ID',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-          TextField(
-            controller: textController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter ID',
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+      child: AlertDialog(
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          spacing: 16,
+          children: [
+            Text(
+              'Download Workshop Mod by ID',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                onPressed: downloading.value
-                    ? null
-                    : () {
-                        Navigator.of(context).pop();
-                      },
-                child: const Text('Cancel'),
+            TextField(
+              controller: textController,
+              cursorColor: Colors.black,
+              keyboardType: TextInputType.number,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                border: OutlineInputBorder(),
+                hintText: 'Enter ID',
               ),
-              ElevatedButton.icon(
-                onPressed: downloading.value
-                    ? null
-                    : () async {
-                        try {
-                          final modId = textController.text;
-                          if (textController.text.isEmpty) {
+            ),
+            Text('Save to: ${targetDirectory.value}'),
+            Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: downloading.value
+                      ? null
+                      : () async {
+                          String? dir;
+                          final initialDirectory = p.normalize(
+                              ref.read(directoriesProvider).workshopDir);
+
+                          try {
+                            dir = await FilePicker.platform.getDirectoryPath(
+                              lockParentWindow: true,
+                              initialDirectory: initialDirectory,
+                            );
+                          } catch (e) {
+                            debugPrint("File picker error $e");
+                            if (context.mounted) {
+                              showSnackBar(
+                                  context, "Failed to open file picker");
+                              Navigator.pop(context);
+                            }
                             return;
                           }
 
-                          downloading.value = true;
+                          if (dir == null) return;
 
-                          final url = Uri.parse(
-                            'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
-                          );
-
-                          final response = await http.post(
-                            url,
-                            body: {
-                              'itemcount': '1',
-                              'publishedfileids[0]': modId.toString(),
-                            },
-                          );
-
-                          final responseData = json.decode(response.body);
-
-                          final fileDetails = responseData['response']
-                              ['publishedfiledetails'][0];
-
-                          final consumerAppId = fileDetails['consumer_app_id'];
-                          if (consumerAppId == 286160) {
-                            final fileUrl = fileDetails['file_url'];
-                            final previewUrl = fileDetails['preview_url'];
-
-                            await downloadAndConvertBson(fileUrl, modId);
-                            await downloadAndResizeImage(previewUrl, modId);
-                            if (context.mounted) {
-                              Navigator.of(context).pop();
+                          targetDirectory.value = p.normalize(dir);
+                        },
+                  child: Text('Select folder'),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: downloading.value
+                      ? null
+                      : () {
+                          Navigator.of(context).pop();
+                        },
+                  child: const Text('Cancel'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: downloading.value
+                      ? null
+                      : () async {
+                          try {
+                            final modId = textController.text;
+                            if (textController.text.isEmpty) {
+                              return;
                             }
-                          } else {
-                            debugPrint(
-                                'Consumer app ID does not match. Expected: 286160, Got: $consumerAppId');
-                          }
-                        } catch (e) {
-                          debugPrint('Error: $e');
 
-                          // TODO add show snackbar for errors
-                        } finally {
-                          downloading.value = false;
-                        }
-                      },
-                icon: Icon(Icons.download),
-                label: const Text('Download'),
-              ),
-            ],
-          ),
-        ],
+                            downloading.value = true;
+
+                            final url = Uri.parse(
+                              'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
+                            );
+
+                            final response = await http.post(
+                              url,
+                              body: {
+                                'itemcount': '1',
+                                'publishedfileids[0]': modId.toString(),
+                              },
+                            );
+
+                            final responseData = json.decode(response.body);
+
+                            final fileDetails = responseData['response']
+                                ['publishedfiledetails'][0];
+
+                            final consumerAppId =
+                                fileDetails['consumer_app_id'];
+                            if (consumerAppId == 286160) {
+                              final fileUrl = fileDetails['file_url'];
+                              final previewUrl = fileDetails['preview_url'];
+
+                              await downloadAndConvertBson(fileUrl, modId);
+                              await downloadAndResizeImage(previewUrl, modId);
+                              if (context.mounted) {
+                                Navigator.of(context).pop();
+                              }
+                            } else {
+                              debugPrint(
+                                  'Consumer app ID does not match. Expected: 286160, Got: $consumerAppId');
+                            }
+                          } catch (e) {
+                            debugPrint('Error: $e');
+
+                            // TODO add show snackbar for errors
+                          } finally {
+                            downloading.value = false;
+                          }
+                        },
+                  icon: Icon(Icons.download),
+                  label: const Text('Download'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

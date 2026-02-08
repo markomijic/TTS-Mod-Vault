b9505ee51090dfa40a88c2ce0a5022df870e2ac7
Marko Mijić <markomijic4@gmail.com>
2026-02-08 09:15:10 +0100

    Add missing main changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 import 'package:hive_ce_flutter/hive_flutter.dart' show Hive, HiveX;
 import 'package:hooks_riverpod/hooks_riverpod.dart' show ProviderScope;
 import 'package:window_manager/window_manager.dart'
@@ -9,11 +10,12 @@ import 'src/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await Hive.initFlutter('TTS Mod Vault-beta');
 
   WindowOptions windowOptions = const WindowOptions(
-    minimumSize: Size(1280, 720),
-    title: 'TTS Mod Vault 1.4.0-beta3',
+    minimumSize: Size(854, 480),
+    title: 'TTS Mod Vault 1.4.0-beta4',
     center: true,
   );
 
